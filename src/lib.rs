365cd06fc48ebb78186d357cd5f385dbe1583851
Rijul Gulati <rijul@rijulgulati.com>
2022-05-23 16:12:11 +0530

    Fixed buy_app and added default apps on initialisation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use near_sdk::{
     borsh::{self, BorshDeserialize, BorshSerialize},
     collections::TreeMap,
-    env, near_bindgen,
+    env,
+    json_types::U128,
+    near_bindgen,
     serde::{Deserialize, Serialize},
     AccountId, PanicOnDefault, Promise,
 };
 
-#[derive(Default, BorshSerialize, BorshDeserialize, Debug, Serialize, Deserialize)]
+#[derive(PanicOnDefault, BorshSerialize, BorshDeserialize, Debug, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
 pub struct App {
     id: u64,
@@ -14,7 +16,7 @@ pub struct App {
     genre: String,
     price: u128,
     published_on: u64,
-    developer: String,
+    developer: AccountId,
 }
 
 #[derive(BorshSerialize, BorshDeserialize, PanicOnDefault)]
@@ -26,19 +28,74 @@ pub struct AppStore {
 #[near_bindgen]
 impl AppStore {
     #[init]
-    pub fn new() -> Self {
+    pub fn new(developer_account: AccountId) -> Self {
         Self {
-            apps: TreeMap::new(b"t"),
+            apps: AppStore::init_apps(developer_account),
         }
     }
 
-    pub fn publish_app(&mut self, title: String, genre: String, yocto_price: String) -> bool {
-        let price = match yocto_price.parse::<u128>() {
-            Ok(p) => p,
-            Err(e) => {
-                env::panic_str(e.to_string().as_str());
-            }
-        };
+    fn init_apps(developer_account: AccountId) -> TreeMap<u64, App> {
+        let mut apps: TreeMap<u64, App> = TreeMap::new(b"t");
+        apps.insert(
+            &1,
+            &App {
+                id: 1,
+                title: "Pokemon".to_string(),
+                genre: "games".to_string(),
+                price: 1_000_000_000_000_000_000_000_000,
+                published_on: env::block_timestamp_ms(),
+                developer: developer_account.clone(),
+            },
+        );
+        apps.insert(
+            &2,
+            &App {
+                id: 2,
+                title: "GTA V".to_string(),
+                genre: "games".to_string(),
+                price: 2_000_000_000_000_000_000_000_000,
+                published_on: env::block_timestamp_ms(),
+                developer: developer_account.clone(),
+            },
+        );
+        apps.insert(
+            &3,
+            &App {
+                id: 3,
+                title: "Uncharted".to_string(),
+                genre: "games".to_string(),
+                price: 3_000_000_000_000_000_000_000_000,
+                published_on: env::block_timestamp_ms(),
+                developer: developer_account.clone(),
+            },
+        );
+        apps.insert(
+            &4,
+            &App {
+                id: 4,
+                title: "Telegram".to_string(),
+                genre: "entertainment".to_string(),
+                price: 4_000_000_000_000_000_000_000_000,
+                published_on: env::block_timestamp_ms(),
+                developer: developer_account.clone(),
+            },
+        );
+        apps.insert(
+            &5,
+            &App {
+                id: 5,
+                title: "Discord".to_string(),
+                genre: "entertainment".to_string(),
+                price: 5_000_000_000_000_000_000_000_000,
+                published_on: env::block_timestamp_ms(),
+                developer: developer_account,
+            },
+        );
+        apps
+    }
+
+    pub fn publish_app(&mut self, title: String, genre: String, yocto_price: U128) -> bool {
+        let price = yocto_price.0;
 
         if env::current_account_id() == env::signer_account_id() {
             env::panic_str("Store itself cannot publish application");
@@ -71,7 +128,7 @@ impl AppStore {
                     price,
                     id,
                     published_on: env::block_timestamp_ms(),
-                    developer: env::signer_account_id().to_string(),
+                    developer: env::signer_account_id(),
                 },
             )
             .is_none()
@@ -87,7 +144,7 @@ impl AppStore {
     // // 2. Store gets 50% of app price
     // // 3. Remaining 50% goes to app owner
     #[payable]
-    pub fn buy_app(&mut self, id: u64) -> bool {
+    pub fn buy_app(&mut self, id: u64) -> Promise {
         if env::current_account_id() == env::signer_account_id() {
             env::panic_str("appstore itself cannot buy apps");
         }
@@ -112,11 +169,24 @@ impl AppStore {
             );
         }
 
+        let mut amount_to_return = 0;
+
+        if deposit > app.price {
+            amount_to_return = deposit - app.price;
+        }
+
         let store_fee = app.price / 2;
         let developer_fee = app.price - store_fee;
-        Promise::new(AccountId::new_unchecked(app.developer.to_string())).transfer(developer_fee);
-        Promise::new(env::current_account_id()).transfer(store_fee);
-        true
+
+        let mut p1 = Promise::new(AccountId::new_unchecked(app.developer.to_string()))
+            .transfer(developer_fee);
+
+        if amount_to_return > 0 {
+            let p2 = Promise::new(env::signer_account_id()).transfer(amount_to_return);
+            p1 = p1.then(p2);
+        }
+
+        p1
     }
 }
 
@@ -126,48 +196,27 @@ mod tests {
 
     use super::*;
 
-    fn get_context(predecessor: AccountId, attach_deposit: bool) -> VMContextBuilder {
+    fn get_context() -> VMContextBuilder {
         let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        if attach_deposit {
-            builder.attached_deposit(5 * near_sdk::ONE_NEAR);
-        }
+        let developer_account = AccountId::new_unchecked("developer.near".to_string());
+        builder.signer_account_id(developer_account.clone());
+        builder.predecessor_account_id(developer_account);
         builder
     }
 
     #[test]
     fn publish_app() {
-        let predecessor_account = AccountId::new_unchecked("alice.near".to_string());
-        let context = get_context(predecessor_account, false);
+        let context = get_context();
         testing_env!(context.build());
 
-        let mut store = AppStore::new();
+        let mut store = AppStore::new(env::signer_account_id());
+        assert_eq!(store.list_apps().len(), 5);
         let result = store.publish_app(
-            "Pokemon".to_string(),
+            "Red Dead Redemption".to_string(),
             "games".to_string(),
-            (5 * near_sdk::ONE_NEAR).to_string(),
+            U128(8 * near_sdk::ONE_NEAR),
         );
         assert!(result);
-
-        let apps = store.list_apps();
-        assert_eq!(1, apps.len());
-    }
-
-    #[test]
-    fn buy_app() {
-        let predecessor_account = AccountId::new_unchecked("alice.near".to_string());
-        let context = get_context(predecessor_account, true);
-        testing_env!(context.build());
-
-        let mut store = AppStore::new();
-        let result = store.publish_app(
-            "Pokemon".to_string(),
-            "games".to_string(),
-            (3 * near_sdk::ONE_NEAR).to_string(),
-        );
-        assert!(result);
-
-        let buy_result = store.buy_app(1);
-        assert!(buy_result);
+        assert_eq!(6, store.list_apps().len());
     }
 }
